a280fade1b0aff86c7bbf3a64bcac17d87754e53
Italo239 <161709454+Italo239@users.noreply.github.com>
2024-03-14 08:38:28 -0300

    Add files via upload

---
+++ aula03/metodos.js
@@ -0,0 +1,12 @@
+let txt = "Hello amigos";
+
+console.log(txt.length)
+
+console.log(txt.slice(6, 12))
+
+console.log(txt.replace("Hello", "olá"));
+
+let newTxt = txt.replace("Hello","olá");
+
+console.warn(newTxt.toUpperCase());
+
